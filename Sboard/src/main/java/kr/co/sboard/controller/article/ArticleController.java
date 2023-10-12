597194f123351b121ee8925563124c45224ed437
31daylee <gussla2206@gmail.com>
2023-10-12 17:45:26 +0900

    게시판 Spring 글작성 및 출력까지 완료

--- Sboard/src/main/java/kr/co/sboard/controller/article/ArticleController.java
+++ Sboard/src/main/java/kr/co/sboard/controller/article/ArticleController.java
@@ -2,11 +2,15 @@ package kr.co.sboard.controller.article;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kr.co.sboard.dto.ArticleDTO;
+import kr.co.sboard.entity.ArticleEntity;
 import kr.co.sboard.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ArticleController {
@@ -15,7 +19,9 @@ public class ArticleController {
     private ArticleService articleService;
 
     @GetMapping("/article/list")
-    public String list(){
+    public String list(Model model, String cate, @RequestParam(defaultValue = "1") int pg){
+        Page<ArticleEntity> pageArticle = articleService.findByParent(pg);
+        model.addAttribute("pageArticle", pageArticle);
         return "/article/list";
     }
 
@@ -31,5 +37,7 @@ public class ArticleController {
         articleService.save(dto);
         return "redirect:/article/list";
     }
+
+
 
 }
--- Sboard/src/main/java/kr/co/sboard/dto/ArticleDTO.java
+++ Sboard/src/main/java/kr/co/sboard/dto/ArticleDTO.java
@@ -2,6 +2,7 @@ package kr.co.sboard.dto;
 
 import kr.co.sboard.entity.ArticleEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -19,7 +20,8 @@ public class ArticleDTO {
     private String cate;
     private String title;
     private String content;
-    private String fname;
+    private int file;
+    private MultipartFile fname;
     private int hit;
     private String writer;
     private String regip;
@@ -35,6 +37,7 @@ public class ArticleDTO {
                 .cate(cate)
                 .title(title)
                 .content(content)
+                .file(file)
                 .hit(hit)
                 .writer(writer)
                 .regip(regip)
